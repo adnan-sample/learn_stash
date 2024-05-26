d1ec1fd35c74365b118f97666e49acaddaa8de00
adnan-sample <165058811+adnan-sample@users.noreply.github.com>
2024-05-26 15:34:27 +0530

    Update logout.py

--- logout.py
+++ logout.py
@@ -2,9 +2,18 @@ import getpass
 
 """
 logout funcionality
+
+author: Muhammed Adnan on May 10th, 2024
+purpose: login user
+paramaeters: username, password
+return: status
+updated by: - sreethu on May 15th, 2024 - Add forget password funcinality
 """
 
 username = input("Enter Username: ")
 password = getpass.getpass("Enter your password: ")
+
+def forgetPasswrod(email: str) -> bool:
+  return True if email.exist() else False
 
 print(f"username: {username}, password: {password}")
