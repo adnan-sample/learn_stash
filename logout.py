a9210ce28a6f33626ed8b5986157ddd262f35745
adnan-sample <165058811+adnan-sample@users.noreply.github.com>
2024-05-26 15:27:10 +0530

    Create logout.py

---
+++ logout.py
@@ -0,0 +1,10 @@
+import getpass
+
+"""
+logout funcionality
+"""
+
+username = input("Enter Username: ")
+password = getpass.getpass("Enter your password: ")
+
+print(f"username: {username}, password: {password}")
